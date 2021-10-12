e2fd6ebf94f553af668c89e66e404c32b98b5b10
vladyslavdmytrenko <vladyslavdmytrenko96@gmail.com>
2021-10-12 23:29:34 +0300

    arrays: wrote body for filterBy, passed tests for this and began write reduceTo

--- src/arrays/arrays.js
+++ src/arrays/arrays.js
@@ -1,29 +1,50 @@
 export function mapTo() {
   // TODO:
-  throw 'Not implemented';
+  throw "Not implemented";
 }
 
 export function mapToProfile() {
   // TODO:
-  throw 'Not implemented';
+  throw "Not implemented";
 }
 
-export function filterBy() {
-  // TODO:
-  throw 'Not implemented';
+export function filterBy(array, filterParam) {
+  switch (typeof filterParam) {
+    case "number":
+      return array.filter((el) => el >= filterParam);
+
+    case "string":
+      return array.filter((el) => el.hasOwnProperty(filterParam));
+
+    case "object":
+      return array.filter((el) => {
+        if (!el.hasOwnProperty(filterParam.property)) return false;
+        return filterParam.filterCb(el[filterParam.property]);
+      });
+  }
 }
 
-export function reduceTo() {
-  // TODO:
-  throw 'Not implemented';
+export function reduceTo(array, reduceParam) {
+  switch (typeof reduceParam) {
+    case "undefined":
+      return array.reduce((prevVal, currVal) => (prevVal += currVal));
+
+    case "string":
+      return array.reduce(
+        (prevVal, currVal) => (prevVal += currVal[reduceParam])
+      );
+
+    case "object":
+      return array.reduce((prevVal, currVal) => {}, []);
+  }
 }
 
 export function sort() {
   // TODO:
-  throw 'Not implemented';
+  throw "Not implemented";
 }
 
 export function complex() {
   // TODO:
-  throw 'Not implemented';
+  throw "Not implemented";
 }
