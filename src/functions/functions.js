dd18efc8a1882e706334bd65f7fff2d3ed5840ef
vladyslavdmytrenko <vladyslavdmytrenko96@gmail.com>
2021-10-12 13:19:12 +0300

    functions: added body for counter function and passed test

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -1,14 +1,49 @@
-export function counter() {
-  // TODO:
-  throw 'Not implemented';
-}
+export const counter = (function () {
+  const counters = {};
+  let result;
+
+  return function (value, name) {
+    switch (typeof value) {
+      case "undefined":
+        if (!counters.hasOwnProperty("default")) {
+          counters.default = 0;
+        } else {
+          counters.default++;
+        }
+        result = counters.default;
+        break;
+
+      case "number":
+        if (name === undefined) {
+          counters.default = value;
+          result = counters.default;
+        }
+
+        if (typeof name === "string") {
+          counters[name] = value;
+          result = counters[name];
+        }
+        break;
+
+      case "string":
+        delete counters.default;
+        if (!counters.hasOwnProperty(value)) {
+          counters[value] = 0;
+        } else {
+          counters[value]++;
+        }
+        result = counters[value];
+    }
+    return result;
+  };
+})();
 
 export function callableMultiplier() {
   // TODO:
-  throw 'Not implemented';
+  throw "Not implemented";
 }
 
 export function createCalculator() {
   // TODO:
-  throw 'Not implemented';
+  throw "Not implemented";
 }
