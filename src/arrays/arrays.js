2b7a7554ba0c8fa1d310cd6d7ddbd472d36bda2d
vladyslavdmytrenko <vladyslavdmytrenko96@gmail.com>
2021-10-13 11:39:29 +0300

    arrays: wrote body for mapTo, mapToProfile and finished reduceTo

--- src/arrays/arrays.js
+++ src/arrays/arrays.js
@@ -1,11 +1,47 @@
-export function mapTo() {
-  // TODO:
-  throw "Not implemented";
+export function mapTo(array, param) {
+  switch (typeof param) {
+    case "undefined":
+      return array.map((el, index) => index);
+
+    case "string":
+      return array.reduce((result, currentValue) => {
+        if (currentValue[param]) result.push(currentValue[param]);
+        return result;
+      }, []);
+  }
 }
 
-export function mapToProfile() {
-  // TODO:
-  throw "Not implemented";
+export function mapToProfile(array) {
+  return array.map((el) => {
+    const age = el.age ? el.age : null;
+    const name = el.name ? el.name : null;
+    const surname = el.surname ? el.surname : null;
+    const fullname =
+      name || surname
+        ? `${name ? name : "_"} ${surname ? surname : "_"}`
+        : null;
+    const result = {
+      name,
+      surname,
+      fullname,
+      age,
+    };
+
+    Object.defineProperties(result, {
+      isOld: {
+        get: function () {
+          return this.age ? (this.age >= 65 ? true : false) : false;
+        },
+      },
+      isAnonymous: {
+        get: function () {
+          return this.fullname ? false : true;
+        },
+      },
+    });
+
+    return result;
+  });
 }
 
 export function filterBy(array, filterParam) {
@@ -31,11 +67,19 @@ export function reduceTo(array, reduceParam) {
 
     case "string":
       return array.reduce(
-        (prevVal, currVal) => (prevVal += currVal[reduceParam])
+        (prevVal, currVal) => (prevVal += currVal[reduceParam]),
+        0
       );
 
     case "object":
-      return array.reduce((prevVal, currVal) => {}, []);
+      return array.reduce(
+        (prevVal, currVal) => {
+          prevVal[0] += currVal[reduceParam[0]];
+          prevVal[1] += currVal[reduceParam[1]];
+          return prevVal;
+        },
+        [0, 0]
+      );
   }
 }
 
